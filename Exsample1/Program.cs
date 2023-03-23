e9e2216c52fb8579e83bb199dea37be245d62264
Stanislsav <Sculkoskij@gmail.ru>
2023-03-23 09:57:11 +0300

    Задача 43

---
+++ Exsample1/Program.cs
@@ -0,0 +1,35 @@
+﻿ // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write ($" Введите размер массива ");
+int i = Convert.ToInt32 (Console.ReadLine ());
+int[] newArray = newint();
+Console.WriteLine(string.Join(" ", newArray));
+Console.Write ($" Чисел больше 0 ввёл пользователь = {count ()}");
+
+int[] newint()
+{
+     int[] array = new int[i];
+     for (int j = 0; j < i; j++)
+     {
+        Console.WriteLine($"Введите число с индексом {j}");
+        int number = Convert.ToInt32 (Console.ReadLine ());
+        array[j] = number;
+     }
+     return array;
+}
+
+int count ()
+{
+    int cou = 0;
+    int[] array = newArray;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j] > 0)
+        {
+            cou += 1;
+        }
+    }
+    return cou;
+}
---
+++ Exsample2/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух
+//прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0,5; -0,5)
+
+int Enter(string mes)
+{
+    Console.WriteLine(mes);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+bool Define(double a, double b, double c, double d)
+{
+    return !(a == b); 
+}
+
+
+(double, double) FundPoint (double a, double b, double c, double d)
+{
+    double x = (d - c)/(a - b);
+    double y = a * (d - c) / (a - b) + c; 
+    return (x, y);
+}
+
+int b1 = Enter("Введите свободный член b1");
+int k1 = Enter("Введите угловой коэффициент k1");
+int b2 = Enter("Введите свободный член b2");
+int k2 = Enter("Введите угловой коэффициент k2");
+
+if (Define(k1, k2, b1, b2) == true)
+{
+    (double x, double y) = FundPoint(k1, k2, b1, b2);
+    Console.WriteLine($"Точка пересечения находится по координатам: ({x}, {y})");
+}
+else
+{
+    Console.WriteLine("Прямые не пересекаются");
+}
